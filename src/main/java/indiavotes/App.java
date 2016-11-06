831d8b43612e780f98a1d65421056e3e71980978
Tanveer-sk <phinix505@gmail.com>
2016-11-06 10:00:20 +0530

    app.java is ready

--- src/main/java/indiavotes/App.java
+++ src/main/java/indiavotes/App.java
@@ -1,10 +1,11 @@
 package indiavotes;
-
+import org.openqa.selenium.WebDriver;
 public class App {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		WebDriver driver;
+		
 	}
 
 }
